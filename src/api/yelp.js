a38e7233c577653633998263e529579971ab35f8
Conrad Domagala <73073121+cd-1578@users.noreply.github.com>
2023-01-09 14:56:12 -0600

    Removed Bearer Token

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL:'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer _zLAowEqPbx6SI50Nv1qTbabAxujVmNdZPkquTEdej1ES6xXsbqyeNhzfNBhkAcjN7gnqbSZrwIRIEfJGJmGkHobAbn2vliSIwjlZ4oWXFo1rg4f3NDzRazkGo9yY3Yx'
+        Authorization: 'Bearer Insert Token'
     }
 })
